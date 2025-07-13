94ea1ac52703a2eb7f081c538288f7fc04c7c278
DivergentClouds <27089165+DivergentClouds@users.noreply.github.com>
2025-07-13 14:02:16 -0500

    wrote common

--- src/common.zig
+++ src/common.zig
@@ -8,3 +8,56 @@ pub const FileAddress = struct {
     file: std.fs.File,
     address: u64,
 };
+
+pub fn ScalarIterator(comptime T: type) type {
+    return struct {
+        buffer: []const T,
+        index: usize,
+
+        const Self = @This();
+
+        pub fn init(buffer: []const T) Self {
+            return .{
+                .buffer = buffer,
+                .index = 0,
+            };
+        }
+
+        pub fn next(self: *Self) ?T {
+            const result = self.peek() orelse return null;
+            self.index += 1;
+            return result;
+        }
+
+        pub fn peek(self: Self) ?T {
+            if (self.index >= self.buffer.len) return null;
+            return self.buffer[self.index];
+        }
+
+        pub fn skip(self: *Self) bool {
+            if (self.index == self.buffer.len) return false;
+
+            self.index += 1;
+            return true;
+        }
+
+        pub fn rest(self: Self) []const T {
+            return self.buffer[self.index..];
+        }
+
+        pub fn reset(self: *Self) void {
+            self.index = 0;
+        }
+
+        pub fn previous(self: *Self) ?T {
+            if (self.index == 0) return null;
+
+            self.index -= 1;
+            return self.peek();
+        }
+    };
+}
+
+pub fn scalarIterator(comptime T: type, buffer: []const T) ScalarIterator(T) {
+    return .{ .buffer = buffer, .index = 0 };
+}
